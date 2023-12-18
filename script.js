90ba9ef998eb317c5d049f6643335fad849d11df
Aayush895 <aayush895@gmail.com>
2023-12-18 20:13:09 +0530

    Done with basic game logic

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const parentContainer = document.getElementById('parent-container')
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissor = document.getElementById('scissor')
+const resultContainer = document.getElementById('result-container')
+const rock = document.getElementById('rock').querySelector('img')
+const paper = document.getElementById('paper').querySelector('img')
+const scissor = document.getElementById('scissors').querySelector('img')
+const declaredResult = document.getElementById('declared-result')
+const playAgain = declaredResult.querySelector('button')
+const rockChoice = document.querySelector('.rock')
+const paperChoice = document.querySelector('.paper')
+const scissorChoice = document.querySelector('.scissors')
+const winOrlose = declaredResult.querySelector('h2').querySelector('span')
 
 const elementArr = []
 
@@ -14,38 +21,96 @@ function generateRandomElement(maxElement) {
   return Math.floor(Math.random() * maxElement)
 }
 
-function getCompChoice() {
+function getCompChoice(e) {
   const randomElement = generateRandomElement(elementArrLength)
-  console.log(elementArr[randomElement])
 
-  // Manipulate the parent container and instead show the choices and who won
-  parentContainer.innerHTML = ` 
-  <div id="result-container">
+  parentContainer.style.display = 'none'
+  resultContainer.style.display = 'flex'
 
-    <div id="human-choice">
-      <h4>YOU PICKED</h4>
-      <!-- Place the pick human chose through js -->
-      <div class="rock">
-        <img src="assets/rock.png" alt="rock">
-      </div>
-    </div>
+  resultContainer.querySelector('#human-choice').innerHTML = `
+        <h4>YOU PICKED</h4>
+        <div class=${e.composedPath()[1].id}>
+          <img src="assets/${e.composedPath()[1].id}.png" alt="${
+    e.composedPath()[1].id
+  }">
+        </div>`
 
-    <div id="declared-result">
-      <h2>YOUR WIN <span>AGAINST PC</span></h2>
-      <button>PLAY AGAIN</button>
-    </div>
-
-    <div id="pc-choice">
+  resultContainer.querySelector('#pc-choice').innerHTML = `
       <h4>PC PICKED</h4>
-      <!-- Place the pick pc chose through js -->
       <div class=${elementArr[randomElement]}>
         <img src="assets/${elementArr[randomElement]}.png" alt=${elementArr[randomElement]}>
-      </div>
-    </div>
+      </div>`
 
-  </div>`
+  if (
+    e.composedPath()[1].id == 'rock' &&
+    elementArr[randomElement] == 'scissors'
+  ) {
+    winOrlose.innerText = 'WIN'
+    console.log('Human won')
+  } else if (
+    e.composedPath()[1].id == 'rock' &&
+    elementArr[randomElement] == 'paper'
+  ) {
+    winOrlose.innerText = 'LOST'
+    console.log('PC won')
+  } else if (
+    e.composedPath()[1].id == 'paper' &&
+    elementArr[randomElement] == 'scissors'
+  ) {
+    winOrlose.innerText = 'LOST'
+    console.log('PC won')
+  } else if (
+    e.composedPath()[1].id == 'paper' &&
+    elementArr[randomElement] == 'rock'
+  ) {
+    winOrlose.innerText = 'WIN'
+    console.log('Human won')
+  } else if (
+    e.composedPath()[1].id == 'scissors' &&
+    elementArr[randomElement] == 'paper'
+  ) {
+    winOrlose.innerText = 'WIN'
+    console.log('Human won')
+  } else if (
+    e.composedPath()[1].id == 'scissors' &&
+    elementArr[randomElement] == 'rock'
+  ) {
+    winOrlose.innerText = 'LOST'
+    console.log('PC won')
+  } else {
+    winOrlose.innerText = 'TIED'
+    console.log('Tied')
+  }
+}
+
+function resetPlayScreen() {
+  parentContainer.style.display = 'grid'
+  resultContainer.style.display = 'none'
 }
 
 rock.addEventListener('click', getCompChoice)
 paper.addEventListener('click', getCompChoice)
 scissor.addEventListener('click', getCompChoice)
+
+playAgain.addEventListener('click', resetPlayScreen)
+
+/**
+ * JS stuff:
+ *
+ * Reset the screen when clicked on 'play again' button --> Done
+ * On clicking the choices, js should display who won and who lost --> Done
+ * On clicking the choices, js should update the counter if human won or if computer won --> TODO
+ * On clicking the rules button user should be able to view the rules and remove it if clicked on the cross button
+ * If user wins, then a next button should be visible beside the rules button otherwise it should be invisible
+ * If user clicks on the next button then they should be redirected towards a victory page
+ *
+ * CSS stuff:
+ *
+ * Have to add the rules and next button.
+ * Have to create a victory page with some basic animations.
+ *
+ * Menial stuff:
+ *
+ * Remove all the comments and console.log
+ *
+ */
